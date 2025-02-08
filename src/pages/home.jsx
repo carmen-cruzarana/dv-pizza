6b598124516453a1bdad9f60bc30d804edeea19e
Carmen Cruz Arana <carmenrubi.arana@gmail.com>
2025-02-08 15:13:15 -0500

    added header to all pages

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,20 +1,12 @@
 import React, {useState} from 'react'
 import { Link } from 'react-router-dom';
+import Header from '../Header.jsx'
 
 function Home() {
     //const [count, setCount] = useState(0)
     return (
         <>
-            <div>
-                <p>
-                    home page
-                </p>
-                <li><Link to="/orderForm">order form</Link></li>
-                <li><Link to="/cart">cart</Link></li>
-                <li><Link to="/menu">menu</Link></li>
-                <li><Link to="/">home</Link></li>
-
-        </div>
+        <Header></Header>
         </>
 
     );
--- src/pages/orderForm.jsx
+++ src/pages/orderForm.jsx
@@ -2,20 +2,13 @@ import viteLogo from "../assets/vite.svg";
 import reactLogo from "../assets/react.svg";
 import React, {useState} from 'react'
 import { Link } from 'react-router-dom'
+import Header from "../Header";
 
 function orderForm() {
     //const [count, setCount] = useState(0)
     return (
         <>
-            <div>
-                <p>
-                    order form
-                </p>
-                <li><Link to="/orderForm">order form</Link></li>
-                <li><Link to="/cart">cart</Link></li>
-                <li><Link to="/menu">menu</Link></li>
-                <li><Link to="/">home</Link></li>
-            </div>
+            <Header/>
         </>
 
     );
