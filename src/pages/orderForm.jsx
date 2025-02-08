67275371ce008bf2e7169919d9d22742e7cd6db2
Jingtong1234 <jingtongzeng123@gmail.com>
2025-02-08 18:35:21 -0500

    Orderform and new variables

--- src/pages/orderForm.jsx
+++ src/pages/orderForm.jsx
@@ -1,137 +1,203 @@
 import {useState} from 'react'
 import { Link } from 'react-router-dom'
 import Header from "../Header";
+import "./style.css";
 
 function OrderForm() {
     //Classes
     const [user, setUser] = useState({
         firstName: "",
         lastName: "",
-        birthday: "",
-        address1: "",
-        address2: "",
         number: "+1",
+        address: "",
+        city: "",
+        state: "",
+        zip: "",
+        email: "",
+        birthday: "",
+
     });
     const[situation, setSituation] = useState({
-        severity: "",
-        involvement: "",
-        weapons: "",
-        discretion: "",
+        size: "",
+        crust: "",
+        sauce: "",
+        meat: "",
     })
+
     // use state functions to update the user input as it comes in
     // const [addedUser, confirmUser] = useState(false);
     // const [addedsituation, confirmSituation] = useState(false);
     const createUser = (type, currValue) =>{
         setUser({...user, [type]: currValue});
     };
-    // const createSituation = (type, currValue) =>{
-    //     setSituation({...user, [type]: currValue});
-    // };
+    const createSituation = (type, currValue) =>{
+        setSituation({...user, [type]: currValue});
+    };
 
     // triggers when user presses add to cart
     const handleSubmit = () =>{
         console.log(user.firstName);
         console.log(user.lastName);
-        console.log(user.address1);
-        console.log(user.address2);
-        console.log(user.birthday);
         console.log(user.number);
+        console.log(user.address);
+        console.log(user.city);
+        console.log(user.state);
+        console.log(user.zip);
+        console.log(user.email);
+        console.log(user.birthday);
         sessionStorage.setItem('User', user);
         sessionStorage.setItem('Situation', situation);
 
     };
     return (
         <>
-            <Header />
-            <div>
-                <p className = "text-4xl font-bold line-height: 500" >
-                    Order Form: Delivery
-                </p>
-                {/*<li><Link to="/orderForm">order form</Link></li>*/}
-                {/*<li><Link to="/cart">cart</Link></li>*/}
-                {/*<li><Link to="/menu">menu</Link></li>*/}
-                {/*<li><Link to="/">home</Link></li>*/}
+            <div className="box-shift-down"></div>
+
+            <div className="border-boxes">
+
+                <Header/>
+                <div>
+                    <h2>
+                        Delivery Information
+                    </h2>
+                    {/*<li><Link to="/orderForm">order form</Link></li>*/}
+                    {/*<li><Link to="/cart">cart</Link></li>*/}
+                    {/*<li><Link to="/menu">menu</Link></li>*/}
+                    {/*<li><Link to="/">home</Link></li>*/}
+                </div>
+                {/*HTML for the input form*/}
+                <div>
+                    <label>
+                        Name:
+                        <input
+                            type="string"
+                            value={user.firstName}
+                            onChange={(e) => {
+                                createUser('firstName', e.target.value)
+                            }}
+                        />
+                        <input
+                            type="string"
+                            value={user.lastName}
+                            onChange={(e) => {
+                                createUser('lastName', e.target.value)
+                            }}
+                        />
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        Phone number (US):
+                        <input
+                            type="string"
+                            value={user.number}
+                            onChange={(e) => {
+                                createUser('number', e.target.value)
+                            }}
+                        />
+                    </label>
+                </div>
+                <div>
+                    <label>
+
+                    </label>
+                </div>
+
+                <div>
+                    <label>
+                        Address:
+                        <input
+                            type="string"
+                            value={user.address}
+                            onChange={(e) => {
+                                createUser('address', e.target.value)
+                            }}
+                        />
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        city:
+                        <input
+                            type="string"
+                            value={user.city}
+                            onChange={(e) => {
+                                createUser('city', e.target.value)
+                            }}
+                        />
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        state:
+                        <input
+                            type="string"
+                            value={user.state}
+                            onChange={(e) => {
+                                createUser('state', e.target.value)
+                            }}
+                        />
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        zip:
+                        <input
+                            type="string"
+                            value={user.zip}
+                            onChange={(e) => {
+                                createUser('zip', e.target.value)
+                            }}
+                        />
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        <br />Sign Up for Email Rewards!
+
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        email:
+                        <input
+                            type="string"
+                            value={user.email}
+                            maxLength={10}
+                            onChange={(e) => {
+                                createUser('email', e.target.value)
+                            }}
+                        />
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        Birthday:
+                        <input
+                            type="string"
+                            value={user.birthday}
+                            maxLength={10}
+                            onChange={(e) => {
+                                createUser('birthday', e.target.value)
+                            }}
+                        />
+                    </label>
+                </div>
+                {/*end of first box*/}
+
+
+                <Link to="/cart">
+                    <button onClick={handleSubmit}>
+                        Add to Cart
+                    </button>
+                </Link>
+
             </div>
-            {/*HTML for the input form*/}
-            <div>
-                <label>
-                    First Name:
-                    <input
-                        type="string"
-                        value={user.firstName}
-                        onChange={(e) => {
-                            createUser('firstName', e.target.value)
-                        }}
-                    />
-                </label>
-            </div>
-            <div>
-                <label>
-                    Last Name:
-                    <input
-                        type="string"
-                        value={user.lastName}
-                        onChange={(e) => {
-                            createUser('lastName', e.target.value)
-                        }}
-                    />
-                </label>
-            </div>
-            <div>
-                <label>
-                    Birthday:
-                    <input
-                        type="string"
-                        value={user.birthday}
-                        maxLength={10}
-                        onChange={(e) => {
-                            createUser('birthday', e.target.value)
-                        }}
-                    />
-                </label>
-            </div>
-            <div>
-                <label>
-                    Address line 1:
-                    <input
-                        type="string"
-                        value={user.address1}
-                        onChange={(e) => {
-                            createUser('address1', e.target.value)
-                        }}
-                    />
-                </label>
-            </div>
-            <div>
-                <label>
-                    Address line 2:
-                    <input
-                        type="string"
-                        value={user.address2}
-                        onChange={(e) => {
-                            createUser('address2', e.target.value)
-                        }}
-                    />
-                </label>
-            </div>
-            <div>
-                <label>
-                    Phone number:
-                    <input
-                        type="string"
-                        value={user.number}
-                        onChange={(e) => {
-                            createUser('number', e.target.value)
-                        }}
-                    />
-                </label>
-            </div>
-            <Link to="/cart">
-                <button onClick={handleSubmit}>
-                Add to Cart
-                </button>
-            </Link>
         </>
+    // <div class="border-boxes">
+    //
+    // </div>
+
 
     );
 }
