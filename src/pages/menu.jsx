80ec9952873984c518263e442c642c0198a4ec46
Alanis-C3 <acastillo3@ufl.edu>
2025-02-08 19:18:05 -0500

    fixed home page and added header to home page

--- src/pages/menu.jsx
+++ src/pages/menu.jsx
@@ -2,13 +2,13 @@ import viteLogo from "../assets/vite.svg";
 import reactLogo from "../assets/react.svg";
 import React, {useState} from 'react'
 import { Link } from 'react-router-dom';
-import Header from "../Header";
+// import Header from "../Header";
 
 function Menu() {
     //const [count, setCount] = useState(0)
     return (
         <>
-        <Header/>
+        {/* <Header/> */}
         </>
 
     );
