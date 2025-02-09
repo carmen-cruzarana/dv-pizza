150f8fb3d4d5c5083458e2595c6415663f2f1c51
Alanis-C3 <acastillo3@ufl.edu>
2025-02-08 20:07:20 -0500

    finished home page and added new jpg file

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -26,16 +26,19 @@ function Home() {
             {/* Left Column */}
             <div className='grow flex flex-col items-center'>
                 <div>
+                    <div class="box-shift-down">
                     {/* <img className="w-28 mx-auto" src="/src/assets/PizzaDoodle.png" width="100vw" />
                     <br /> */}
+                    {/* <br/>
                     <br/>
-                    <br/>
-                    <br/>
+                    <br/> */}
                     <img  
                     src="/src/assets/Pizza-promo.jpg" width={1000} height={300} />
                     <br />
+                    <img className= "mx-auto" src="/src/assets/pizza-ad.jpg" width={300} height={300}/>
                     {/* <img  
                     src="https://img.freepik.com/free-vector/hand-drawn-pizza-horizontal-banner_23-2150266608.jpg" width={1000} height={300} /> */}
+                </div>
                 </div>
             </div>
 
@@ -45,15 +48,14 @@ function Home() {
                 <br/>
                 <br/>
                 <div class="border-boxes">
-                    <h1>Coupons</h1>
-                    <p> --------------------------------</p>
+                    <h1 style= {{color: "#881ec2"}}>Coupons</h1>
+                    <p style= {{color: "#881ec2"}}> --------------------------------</p>
                     <p>Get 10% off your first order with coupon code: FIRST10</p>
                     <p>Get express delivery with coupon code : EXPRESS</p>
                 </div>
-                <br/>
                 <div class="border-boxes">
                     <h2>Browse our menu</h2>
-                    <p>Click <Link to="/menu">here</Link> to see our menu</p>
+                    <p class="special-address">Click <Link to="/menu">here</Link> to see our menu</p>
                     <p>
                         Specials: <br />
                         Meat lovers $9.99 <br />
@@ -62,14 +64,14 @@ function Home() {
                     </p>
                     <p class="hidden-messages">We are here to help</p>
                 </div>
-                <br/>
+                
                 <div class="border-boxes">
                     <h2> 30-Minute Delivery Guaranteed</h2>
                     <p class="hidden-messages">Ask for express if needed sooner</p>
                 </div>
-                <br/>
+               
                 <div class="border-boxes">
-                    <h2> Our Pizza</h2>
+                    <h2 style={{ fontSize: "24px" }}>Our Pizza</h2>
                     <p font-size="14px">
                         Allergen info <br />
                         Gluten Free warning
